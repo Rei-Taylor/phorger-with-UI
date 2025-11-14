e88682f3934d5907edbb9e83b6db7eb7b26a7fdb
Darius <reza.blersch@gmail.com>
2025-11-14 21:41:26 +0100

    first phorger commit

--- pkg/gdrive_uploader.py
+++ pkg/gdrive_uploader.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import os.path
 import json, logging
-logging.basicConfig(filename="log.txt", filemode='w', level=logging.DEBUG, format='%(message)s')
+
+logging.basicConfig(
+    filename="log.txt", filemode="w", level=logging.DEBUG, format="%(message)s"
+)
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -13,6 +16,7 @@ from googleapiclient.http import MediaFileUpload
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
+
 def main(file_list):
     try:
         service = auth_service()
@@ -20,125 +24,143 @@ def main(file_list):
         file_uploader(service, folder_id, file_list)
         print("upload successful!")
         return
-    
-        #logging.debug("Following Files in Drive")
-        #files = test_call(service)
-        #logging.debug(json.dumps(files, indent=2))
 
-        
+        # logging.debug("Following Files in Drive")
+        # files = test_call(service)
+        # logging.debug(json.dumps(files, indent=2))
+
     except HttpError as error:
         print(f"An error occurred: {error}")
         return None
-    
+
+
 def auth_service():
     creds = None
     if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('client_secrets.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "client_secrets.json", SCOPES
+            )
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as fileobj:
+        with open("token.json", "w") as fileobj:
             fileobj.write(creds.to_json())
-    service = build("drive", "v3", credentials=creds) #returns discovery resource object
+    service = build(
+        "drive", "v3", credentials=creds
+    )  # returns discovery resource object
     return service
+
 
 def test_call(service):
     # Call the Drive v3 API
-    results = ( #returns dict
-        service
-        .files()
-        .list(pageSize=10, fields="nextPageToken, files(id, name)") # endpoint
-        .execute() #makes the actual request on http request object
+    results = (  # returns dict
+        service.files()
+        .list(pageSize=10, fields="nextPageToken, files(id, name)")  # endpoint
+        .execute()  # makes the actual request on http request object
     )
 
-    items = results.get("files", []) #filters out results
+    items = results.get("files", [])  # filters out results
     return items
 
+
+# retrieves the phorger folder
 def folder(service):
-    results = ( #checking for exisiting folder
-        service
-        .files()
-        .list(q="mimeType='application/vnd.google-apps.folder'", fields="files(id, name)") #filters out folder
-        .execute() 
-    )    
+    results = (  # checking for exisiting folder
+        service.files()
+        .list(
+            q="mimeType='application/vnd.google-apps.folder'", fields="files(id, name)"
+        )  # filters out folder
+        .execute()
+    )
     folder = results.get("files", [])
     if folder:
         for file in folder:
             if file["name"] == "phorger":
                 print("phorger folder found!")
-                logging.debug(f"phorger folder found under id: {file["id"]}")
+                logging.debug(f"phorger folder found under id: {file['id']}")
                 return file["id"]
-    
+
     print("phorger folder not found, creating...")
-    file_metadata = {"name": "phorger", "mimeType": "application/vnd.google-apps.folder"}
+    file_metadata = {
+        "name": "phorger",
+        "mimeType": "application/vnd.google-apps.folder",
+    }
     folder = service.files().create(body=file_metadata, fields="id").execute()
-    logging.debug(f"Phorger folder ID: {folder.get("id")}.")
+    logging.debug(f"Phorger folder ID: {folder.get('id')}.")
     print("...success!")
     return folder.get("id")
 
+
+# gets the id of the album folder?
 def album_folder(service, phorger_folder_id, album_name):
     # checking for exisiting folder
     q = f"'{phorger_folder_id}' in parents and mimeType = 'application/vnd.google-apps.folder'"
-    results = ( 
-        service
-        .files()
-        .list(q=q, fields="files(id, name)") # filters out folder
-        .execute() 
-    )    
+    results = (
+        service.files()
+        .list(q=q, fields="files(id, name)")  # filters out folder
+        .execute()
+    )
     folder = results.get("files", [])
     if folder:
         for file in folder:
             if file["name"] == album_name:
-
-                logging.debug(f"album folder found under id: {file["id"]}")
+                logging.debug(f"album folder found under id: {file['id']}")
                 album_id = file["id"]
                 return album_id
-            
+
     # if album folder not found #
 
-    file_metadata = {"name": album_name, "mimeType": "application/vnd.google-apps.folder", "parents": [phorger_folder_id]}
+    file_metadata = {
+        "name": album_name,
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": [phorger_folder_id],
+    }
     folder = service.files().create(body=file_metadata, fields="id").execute()
-    logging.debug(f"album folder created, id: {folder.get("id")}.")
+    logging.debug(f"album folder created, id: {folder.get('id')}.")
 
     album_id = folder.get("id")
     return album_id
 
-def file_uploader(service, folder_id, file_list):
 
+def file_uploader(service, folder_id, file_list):
     counter = 0
     phorger_folder_id = folder_id
     for path_object in file_list:
         album_id = album_folder(service, phorger_folder_id, path_object.name)
         output_file_path = path_object / "output"
-        for file in output_file_path.glob("*.jpg"): #uploading only jpgs found in output folder
-
+        for file in output_file_path.glob(
+            "*.jpg"
+        ):  # uploading only jpgs found in output folder
             print(f"uploading {file}")
             logging.debug(f"output file: {file}")
 
-            
             file_metadata = {"name": file.name, "parents": [album_id]}
-            media = MediaFileUpload(str(file), mimetype="image/jpeg") # converting path object to string
+            media = MediaFileUpload(
+                str(file), mimetype="image/jpeg"
+            )  # converting path object to string
 
             file = (
                 service.files()
-                .create(body=file_metadata, media_body=media, fields="id", uploadType="multipart")
+                .create(
+                    body=file_metadata,
+                    media_body=media,
+                    fields="id",
+                    uploadType="multipart",
+                )
                 .execute()
             )
             counter += 1
-            logging.debug(f'File ID: {file.get("id")}')
+            logging.debug(f"File ID: {file.get('id')}")
 
         return file.get("id"), print(f"files uploaded: {counter}"), album_id
 
+
 def link_share(service, file):
-    
     # TODO
     pass
-
-
-
 
 
 if __name__ == "__main__":
--- pkg/photomover.py
+++ pkg/photomover.py
@@ -1,72 +1,92 @@
 import shutil, os, sys, logging, time
 from pathlib import Path
-from tqdm import tqdm
 
-logging.basicConfig(filename="log.txt", filemode='w', level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
+# from tqdm import tqdm
 
-def main(source_list: list,destination: Path, size: int, confrimation: bool):
-    logging.debug(f'''   
+logging.basicConfig(
+    filename="log.txt",
+    filemode="w",
+    level=logging.DEBUG,
+    format=" %(asctime)s -  %(levelname)s -  %(message)s",
+)
+
+
+def main(source_list: list, destination: Path, size: int, confrimation: bool):
+    logging.debug(
+        f"""
         file mover initialized\n
         source list:\n{source_list}
-        destination:\n{destination}         
-    ''')
-    
+        destination:\n{destination}
+    """
+    )
+
     path_validator(destination, size)
     files_to_move = duplicate_checker(source_list, destination)
 
-    if confrimation == True: #confirmation dialogue before moving files
+    if confrimation == True:  # confirmation dialogue before moving files
         while True:
-            user_input = input('files ready for migrating, press y to confirm or exit:')
-            if user_input == 'y':
+            user_input = input("files ready for migrating, press y to confirm or exit:")
+            if user_input == "y":
                 move_execution(files_to_move, destination)
                 break
-            elif user_input == 'exit':
-                print('program cancelled, exiting...')
+            elif user_input == "exit":
+                print("program cancelled, exiting...")
                 sys.exit(0)
             else:
-                print('invalid command, try again')
+                print("invalid command, try again")
     else:
         move_execution(files_to_move, destination)
 
-def path_validator(destination: Path, size: int):
-    if not destination.is_dir(): #checking destination folder
-        raise Exception('destination folder not found, check directory or settings')
-    total, used, free = shutil.disk_usage(destination.anchor) #checking for available space on destination drive
-    print(f'available space on destination drive {free / (1024 ** 3):.2f} GB')
-    if free < size:
-        raise Exception('not enough disk space, exiting')
 
-    return print('destination valid, proceeding...')
+def path_validator(destination: Path, size: int):
+    if not destination.is_dir():  # checking destination folder
+        raise Exception("destination folder not found, check directory or settings")
+    total, used, free = shutil.disk_usage(
+        destination.anchor
+    )  # checking for available space on destination drive
+    print(f"available space on destination drive {free / (1024**3):.2f} GB")
+    if free < size:
+        raise Exception("not enough disk space, exiting")
+
+    return print("destination valid, proceeding...")
 
 
 def duplicate_checker(source_list: list, destination: Path) -> list:
-    print('checking for duplicates...') #checking for existing folder in destination
-    duplicates = [file for file in destination.iterdir() if file.name == file.name for file in source_list]
-    logging.debug(f'duplicate list:\n{duplicates}')
+    print("checking for duplicates...")  # checking for existing folder in destination
+    duplicates = [
+        file
+        for file in destination.iterdir()
+        if file.name == file.name
+        for file in source_list
+    ]
+    logging.debug(f"duplicate list:\n{duplicates}")
     if duplicates:
-        print('duplicate folder found at destination:')
+        print("duplicate folder found at destination:")
         for duplicate_file in duplicates:
             print(duplicate_file.name)
-        files_to_move = [file for file in source_list if file not in duplicates] #adjusting the source file list to exclude duplicates
+        files_to_move = [
+            file for file in source_list if file not in duplicates
+        ]  # adjusting the source file list to exclude duplicates
         if not files_to_move:
-            raise Exception('Files already exist at destination, exiting')
+            raise Exception("Files already exist at destination, exiting")
     else:
-        print('no duplicates found, proceeding...')
+        print("no duplicates found, proceeding...")
         files_to_move = source_list
 
-    logging.debug(f'files to move:\n{files_to_move}')
+    logging.debug(f"files to move:\n{files_to_move}")
     return files_to_move
 
 
-def move_execution(files_list: list, destination: Path): #actually moves the files
+def move_execution(files_list: list, destination: Path):  # actually moves the files
     files_moved = 0
     for file in files_list:
-        print(f'moving {file.name}...')
+        print(f"moving {file.name}...")
         shutil.move(file, destination)
         files_moved += 1
-    return print(f'successfully moved {files_moved} file(s)!')
+    return print(f"successfully moved {files_moved} file(s)!")
 
-# def move_file_with_progress(file, destination, chunk_size=1024*1024): 
+
+# def move_file_with_progress(file, destination, chunk_size=1024*1024):
 #     total_size = os.path.getsize(file)
 #     start_time = time.time()
 #     moved_bytes = 0
@@ -74,7 +94,7 @@ def move_execution(files_list: list, destination: Path): #actually moves the fil
 #     with open(file, 'rb') as fsrc, open(destination, 'wb') as fdst, tqdm(
 #         total=total_size,
 #         unit='B',
-#         unit_scale=True, 
+#         unit_scale=True,
 #         unit_divisor=1024,
 #         desc=f"Moving {os.path.basename(file)}",
 #     ) as bar:
@@ -89,7 +109,6 @@ def move_execution(files_list: list, destination: Path): #actually moves the fil
 #             bar.set_postfix_str(f"{speed/1024/1024:.2f} MB/s")
 #             bar.update(len(chunk))
 #     os.remove(file)
-
 
 
 if __name__ == "__main__":
